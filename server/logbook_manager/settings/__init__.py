65b9faa1f6e10837b120b6817f7c748a4ea2e33b
Zachary Waweru <azwdevops@gmail.com>
2025-03-31 12:12:16 +0300

    chore: added server.py to align with vercel's serverless functions, add shebang in build_files.sh top to ensure it is a commands file, also made it executable, added STATICFILES_STORAGE to point to whitenoise

--- server/logbook_manager/settings/__init__.py
+++ server/logbook_manager/settings/__init__.py
@@ -130,7 +130,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "build/static"]
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ server/server.py
@@ -0,0 +1,3 @@
+from logbook_manager.wsgi import application
+
+app = application  # Vercel expects a callable named 'app'
